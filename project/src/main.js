88a299334ee7f16f7c95d67a6c9dea86c56c96a1
yeongbin <yeongbin6483@gmail.com>
2024-09-12 15:03:05 +0900

    부트스트랩 사용하기

--- project/src/main.js
+++ project/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import PageTitle from './components/fragments/PageTitle.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 const app = createApp(App)
 
--- project/src/views/4_reuse/PageView.vue
+++ project/src/views/4_reuse/PageView.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
+  <div class="container">
     <page-title title="고객목록" />
+    <h3 style="border-left: 5px solid red; padding-left: 3px; text-align: left">
+      고객목록
+    </h3>
+    <button class="btn btn-danger">클릭</button>
   </div>
 </template>
 <script>
