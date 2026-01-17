de4b0dffe81ce76c753681e0250c3987468a626d
yeongbin <yeongbin6483@gmail.com>
2026-01-17 22:16:09 +0900

    강의 다시 따라하는버전 커밋, 섹션1. 8.라이프사이클 훅 까지 한 것

---
+++ project/src/views/5_advanced/MixinView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div></div>
+</template>
+<script>
+import Formatter from '@/mixins/formatter'
+// import Axios from '@/mixins/axios'
+export default {
+  components: {},
+  mixins: [Formatter],
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  created() {
+    console.log('MixinView의 created')
+  },
+  mounted() {
+    console.log('MixinView의 mounted')
+    console.log(this.$convertDateFormat('20220601', 'YYYY-MM-DD'))
+    console.log(this.$convertDateFormat('20220601', 'MM.DD.YYYY'))
+
+    console.log(this.$convertNumberFormat(2547500, '#,###'))
+    console.log(this.$convertNumberFormat(2547500, '#,###.#0'))
+    console.log(this.$convertNumberFormat(2547500.4, '#,###.#0'))
+    console.log(this.$convertNumberFormat(2547500.4, '#,###.##'))
+    console.log(this.$convertNumberFormat(-2547500.4, '#,###.##'))
+    console.log(this.$convertNumberFormat(-2547500.4, '$#,###.##'))
+    console.log(this.$convertNumberFormat(125.23, '#,###.#0%'))
+    console.log(this.$convertNumberFormat(125.5, '#,###.#0%'))
+    console.log(this.$convertNumberFormat(125, '#,###원'))
+
+    this.getCustomers()
+  },
+  unmounted() {},
+  methods: {
+    async getCustomers() {
+      const customers = await this.$get('http://localhost:8080/advanced/mixin')
+      console.log(customers)
+    }
+  }
+}
+</script>
